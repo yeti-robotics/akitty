b23fabf8a035866a054667a50f3f660779f83ba8
Venkata <chappidiv@gmail.com>
2026-01-23 18:43:38 -0500

    git commit

---
+++ src/main/java/frc/robot/subsystems/arm/ArmIOTalonFX.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems.arm;
+
+import static frc.robot.subsystems.arm.ArmConfig.cancoderConfiguration;
+import static frc.robot.subsystems.arm.ArmConfig.primaryTalonFXConfigs;
+
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class ArmIOTalonFX implements ArmIO {
+
+    private final TalonFX armKraken;
+    private final CANcoder armCANCoder;
+    private final MotionMagicTorqueCurrentFOC control = new MotionMagicTorqueCurrentFOC(position);
+
+    public ArmIOTalonFX() {
+        armKraken = new TalonFX(ArmConfig.armKrakenID, RIO_BUS);
+        armCANCoder = new CANcoder(ArmConfig.armCANcoderID, RIO_BUS);
+
+        armKraken.getConfigurator().apply(primaryTalonFXConfigs);
+        armCANCoder.getConfigurator().apply(cancoderConfiguration);
+
+        if (RobotBase.isSimulation()) {
+            PhysicsSim.getInstance().addTalonFX(armKraken);
+            PhysicsSim.getInstance().addCANCoder(armCANCoder);
+        }
+    }
+
+    @Override
+    public void updateInputs(ArmIOInputs inputs) {
+        inputs.positionRotation = armKraken.getPosition().getValueAsDouble();
+        inputs.velocityRPM = armKraken.getVelocity().getValueAsDouble();
+    }
+
+    @Override
+    public void moveToPosition(Angle position) {
+        armKraken.setControl(control.withPosition(position));
+    }
+}
--- src/main/java/frc/robot/subsystems/intakeroller/IntakeRollerConfigTalonFXReal.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.intakeroller;
-
-public class IntakeRollerConfigTalonFXReal {
-    static final int rollerID = 0;
-}
